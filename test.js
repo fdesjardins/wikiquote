69c81911eb7dd920b9ab48ea063d24ad2bd258c9
Forrest Desjardins <desjardinsfg@gmail.com>
2017-08-27 13:54:48 -0800

    add tests for searchPeople, getRandomQuote

--- test.js
+++ test.js
@@ -7,4 +7,18 @@ describe('wikiquote', () => {
   it('should run tests', () => {
     return assert(1 !== 2 && wikiquote !== undefined)
   })
+
+  it('should search for people', () => {
+    return wikiquote.searchPeople('steve jobs')
+      .then(page => {
+        assert(page.title === 'Steve Jobs')
+      })
+  })
+
+  it('should get a random quote', () => {
+    return wikiquote.getRandomQuote('Steve Jobs')
+      .then(quote => {
+        assert(quote.length > 0)
+      })
+  })
 })
